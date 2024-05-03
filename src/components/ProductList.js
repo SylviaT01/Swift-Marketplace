02b0b349fd1aab193c5cf2472103025581d7a137
Sylvia <sylviachebet03@gmail.com>
2024-05-03 22:01:17 +0300

    create categories component for displaying categories

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function ProductList() {
     const [products, setProducts] = useState([])
-    
+
     useEffect(() => {
         fetch('https://dummyjson.com/products?limit=0')
             .then(res => res.json())
@@ -12,24 +12,111 @@ function ProductList() {
 
 
     return (
+        // <section className="mb-8">
+        //     <Link to="/">Home</Link>
+        //     <h2 className="text-xl font-semibold mb-4">Product Catalog</h2>
+        //     <div className="grid grid-cols-3 gap-4">
+        //         {products.map((product, index) => (
+        //             <div key={index} className="border p-4">
+        //                 <h3 className="text-lg font-semibold">{product.title} {product.rating}</h3>
+        //                 <img src={product.thumbnail} alt={product.title} className="w-full mb-4" />
+        //                 <p>Brand: {product.brand}</p>
+        //                 <p>Description: {product.description}</p> 
+        //                 <p>Price: {product.price}</p>
+        //                 <p>Category: {product.category}</p>
+        //                 {/* Add more product details as needed */}
+        //             </div>
+        //         ))}
+        //     </div>
+
+        // </section>
+
         <section className="mb-8">
             <Link to="/">Home</Link>
             <h2 className="text-xl font-semibold mb-4">Product Catalog</h2>
-            <div className="grid grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {products.map((product, index) => (
-                    <div key={index} className="border p-4">
-                        <h3 className="text-lg font-semibold">{product.title} {product.rating}</h3>
-                        <img src={product.thumbnail} alt={product.title} className="w-full mb-4" />
-                        <p>Brand: {product.brand}</p>
-                        <p>Description: {product.description}</p> 
-                        <p>Price: {product.price}</p>
-                        <p>Category: {product.category}</p>
+                    <div key={index} className="border p-4 flex flex-col justify-between shadow-lg shadow-black rounded">
+                        <div className="flex-grow">
+                            <h3 className="text-lg font-semibold mb-2">{product.title}</h3>
+                            <div className="image-container">
+                                <img src={product.thumbnail} alt={product.title} className="w-full h-full mb-2" />
+                            </div>
+                        </div>
+                        <div>
+                            <p className="mb-2"><span className="font-semibold">Brand:</span> {product.brand}</p>
+                            <p className="mb-2"><span className="font-semibold">Description:</span> {product.description}</p>
+                            <p className="mb-2"><span className="font-semibold">Price:</span> ${product.price}</p>
+                            <p className="mb-2"><span className="font-semibold">Category:</span> {product.category}</p>
+                        </div>
                         {/* Add more product details as needed */}
                     </div>
                 ))}
             </div>
-
         </section>
     )
 }
 export default ProductList;
+
+// import React, { useState, useEffect } from 'react';
+// import { Link } from "react-router-dom";
+// import Categories from './categories';
+
+// function ProductList() {
+//     const [products, setProducts] = useState([]);
+//     const [categories, setCategories] = useState([]);
+//     const [selectedCategory, setSelectedCategory] = useState(null);
+
+//     useEffect(() => {
+//         fetch('https://dummyjson.com/products?limit=0')
+//             .then(res => res.json())
+//             .then(data => {
+//                 setProducts(data.products);
+//                 // Extract and set categories
+//                 const uniqueCategories = [...new Set(data.products.map(product => product.category))];
+//                 setCategories(uniqueCategories.map((category, index) => ({ id: index, name: category })));
+//             });
+//     }, []);
+
+//     const filterProductsByCategory = categoryId => {
+//         setSelectedCategory(categoryId);
+//     };
+
+//     const filteredProducts = selectedCategory
+//         ? products.filter(product => product.category === categories[selectedCategory].name)
+//         : products;
+
+//     return (
+//         <section className="mb-8">
+//             <Link to="/">Home</Link>
+//             <h2 className="text-xl font-semibold mb-4">Product Catalog</h2>
+//             <div className="flex-row mb-4">
+//                 <Categories
+//                     categories={categories}
+//                     selectedCategory={selectedCategory}
+//                     onSelectCategory={filterProductsByCategory}
+//                 />
+//             </div>
+//             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+//                 {filteredProducts.map((product, index) => (
+//                     <div key={index} className="border p-4 flex flex-col justify-between shadow-md">
+//                         <div className="flex-grow">
+//                             <h3 className="text-lg font-semibold mb-2">{product.title} {product.rating}</h3>
+//                             <div className="image-container">
+//                                 <img src={product.thumbnail} alt={product.title} className="w-full h-full mb-2" />
+//                             </div>
+//                         </div>
+//                         <div>
+//                             <p className="mb-2"><span className="font-semibold">Brand:</span> {product.brand}</p>
+//                             <p className="mb-2"><span className="font-semibold">Description:</span> {product.description}</p> 
+//                             <p className="mb-2"><span className="font-semibold">Price:</span> {product.price}</p>
+//                             <p className="mb-2"><span className="font-semibold">Category:</span> {product.category}</p>
+//                         </div>
+//                     </div>
+//                 ))}
+//             </div>
+//         </section>
+//     );
+// }
+
+// export default ProductList;
---
+++ src/components/categories.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Categories({ categories, selectedCategory, onSelectCategory }) {
+
+    const capitalizeFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    };
+
+    return (
+        <div>
+            <h3 className="font-semibold mb-2">Categories</h3>
+            <ul className="flex flex-wrap items-center justify-center gap-4 mb-4">
+                {categories.map(category => (
+                    <li
+                        key={category.id}
+                        className={`cursor-pointer border-2 rounded hover:bg-slate-100 p-1${selectedCategory === category.id ? ' font-bold' : ''}`}
+                        onClick={() => onSelectCategory(category.id)}
+                    >
+                        {capitalizeFirstLetter(category.name)}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default Categories;
