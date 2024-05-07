b4bde9655c0b151dcb2bf5cfa715038c71d4d8fa
Sylvia <sylviachebet03@gmail.com>
2024-05-07 19:28:11 +0300

    Add background image to the main container with a cover style,then adjust width for different screen sizes to improve layout responsiveness

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -5,7 +5,8 @@ const ContactForm = () => {
     return (
         <>
         <Navbar />
-            <div className="max-w-md mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="flex items-center justify-center bg-cover h-screen bg-[url('/src/assets/page2.jpg')]">
+            <div className="w-11/12 md:w-2/3 lg:w-1/2 xl:w-1/3 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 <h2 className="text-2xl font-bold mb-6">Contact Us</h2>
                 <form>
                     <div className="mb-4">
@@ -50,6 +51,7 @@ const ContactForm = () => {
                         </button>
                     </div>
                 </form>
+            </div>
             </div>
         </>
     );
