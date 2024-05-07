27cbee8e8cabfea4f3d1ae575a8de2c3fd85f8a1
Sylvia <sylviachebet03@gmail.com>
2024-05-07 23:52:55 +0300

    change color styles for footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
   }, []);
 
   return (
-    <footer className="text-gray-300 bg-black py-20">
+    <footer className="text-black bg-slate-400 py-20">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 pb-20 wrapper w-full gap-16 md:gap-10 lg:gap-5 xl:gap-10 2xl:px-20 ">
         <div className="footer-col-1 flex flex-col items-start">
           <Link
@@ -23,9 +23,9 @@ const Footer = () => {
             data-aos-duration="1000"
             data-aos-delay="0"
             href="/"
-            className="text-4xl md:text-2xl font-semibold text-rose-500 hover:text-white duration-300"
+            className="text-4xl md:text-2xl font-semibold text-rose-400 hover:text-white duration-300"
           >
-            euphoria.
+            Swift MarketPlace
           </Link>
         </div>
         <div
@@ -34,19 +34,19 @@ const Footer = () => {
           data-aos-duration="1500"
           data-aos-delay="300"
         >
-          <p className="follow-text uppercase tracking-wider text-gray-400">{`Don't forget to follow us`}</p>
-          <div className="social-icons">
+          <p className="follow-text uppercase tracking-wider text-gray-900">{`Don't forget to follow us`}</p>
+          <div className="social-icons flex gap-5 p-5">
             <div className="icon-circle">
-              <FaFacebookF className="text-rose-100 mx-auto" />
+              <FaFacebookF className="text-rose-100 mx-auto bg-blue-800" />
             </div>
             <div className="icon-circle">
-              <FaInstagram className="text-rose-100 mx-auto" />
+              <FaInstagram className="text-rose-100 mx-auto bg-pink-400" />
             </div>
             <div className="icon-circle">
-              <FaLinkedinIn className="text-rose-100 mx-auto" />
+              <FaLinkedinIn className="text-rose-100 mx-auto bg-blue-600" />
             </div>
             <div className="icon-circle">
-              <FaTwitter className="text-rose-100 mx-auto" />
+              <FaTwitter className="text-rose-100 mx-auto bg-black" />
             </div>
           </div>
         </div>
@@ -82,10 +82,10 @@ const Footer = () => {
           data-aos-duration="1500"
           data-aos-delay="1000"
         >
-          <p className="uppercase tracking-wider text-gray-400">
-            Need more informations?
+          <p className="uppercase tracking-wider text-gray-900">
+            Need more information?
           </p>
-          <button className="footer-btn bg-rose-700 py-4 px-6 rounded-full uppercase text-sm font-medium hover:text-rose-500 hover:bg-rose-50 duration-300">
+          <button className="footer-btn bg-rose-400 py-4 px-6 rounded-full uppercase text-sm font-medium hover:text-rose-300 hover:bg-rose-50 duration-300">
             + New Message
           </button>
           <p className="font-medium text-lg">contact@swiftmarketplace.com</p>
@@ -93,7 +93,7 @@ const Footer = () => {
       </div>
 
       <p
-        className="copyright border-t border-gray-500/40 pt-20 text-gray-400 wrapper text-center uppercase"
+        className="copyright border-t border-gray-500/40 pt-20 text-gray-900 wrapper text-center uppercase"
         data-aos="zoom-in-up"
         data-aos-duration="800"
         data-aos-delay="1300"
