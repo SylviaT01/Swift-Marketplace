c8ad3644ce88f9a6e419b7bd1eaace2a919f1d1e
Sylvia <sylviachebet03@gmail.com>
2024-05-08 22:43:17 +0300

    handle loginform, feedback and contact forms to redirect user to homepage after successful login

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -3,13 +3,18 @@ import { Navbar } from './navbar';
 import Footer from './footer';
 
 const ContactForm = () => {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        window.location.href = '/'; // Redirect to homepage
+      };
     return (
         <>
         <Navbar />
         <div className="flex items-center justify-center bg-cover h-screen bg-[url('/src/assets/page1.jpeg')]">
             <div className="w-11/12 md:w-2/3 lg:w-1/2 xl:w-1/3 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 <h2 className="section-title text-2xl font-bold mb-6">Contact Us</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="mb-4">
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
                             Name
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -4,13 +4,19 @@ import Footer from './footer';
 
 
 const LoginForm = () => {
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    window.location.href = '/'; // Redirect to homepage
+  };
+
   return (
     <>
     <Navbar />
     <div className="flex items-center justify-center bg-cover h-screen bg-[url('/src/assets/page1.jpeg')]">
       <div className="w-11/12 md:w-2/3 lg:w-1/2 xl:w-1/3 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <h2 className="text-2xl font-bold mb-6">Log In</h2>
-        <form>
+        <form onSubmit={handleLogin}>
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
               Username
