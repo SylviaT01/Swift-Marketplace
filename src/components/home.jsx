b2fb9c45fe91c234c48116f2dd21584ab6815a9f
Sylvia <sylviachebet03@gmail.com>
2024-05-08 12:10:23 +0300

    Add browse products button to each image in the home slider and adjust button styles for better visibility and add hover effects for improved user interaction

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -12,16 +13,17 @@ const Home = () => {
 
     useEffect(() => {
         AOS.init({ duration: 1000 });
+        AOS.refresh();
     }, []);
 
     const settings = {
-        dots: true,
+        // dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        // autoplay: true,
-        // autoplaySpeed: 3000, // Adjust as needed
+        autoplay: true,
+        autoplaySpeed: 3000, // Adjust as needed
     };
 
     const goToNextSlide = () => {
@@ -35,30 +37,39 @@ const Home = () => {
     return (
         <div className="w-full max-w-full overflow-hidden relative">
             <div className="slider-container h-full">
-            <Slider ref={sliderRef} {...settings}>
-                <div className='relative'>
-                    <img src="https://images.pexels.com/photos/6567607/pexels-photo-6567607.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="Woman after shopping" className="w-full h-svh object-cover" />
-                    <div className="absolute inset-0 flex flex-col justify-center items-center text-center text-black">
-                        <h2 data-aos="fade-down" data-aos-duration="1500" className="text-4xl md:text-6xl lg:text-7xl  font-semibold uppercase space-font z-10">Grab yours</h2>
-                        <p data-aos="fade-right"
-                            data-aos-duration="1500"
-                            className="w-2/3 lg:w-1/2 z-20 text-sm md:text-base lg:text-lg xl:text-xl">You get everything you need</p>
+                <Slider ref={sliderRef} {...settings}>
+                    <div className='relative'>
+                        <img src="https://images.pexels.com/photos/6567607/pexels-photo-6567607.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="Woman after shopping" className="w-full h-svh object-cover" />
+                        <div className="absolute inset-0 flex flex-col justify-center items-center text-center text-white">
+                            <h2 data-aos="fade-down" data-aos-duration="1500" className="text-4xl md:text-6xl lg:text-7xl  font-semibold uppercase space-font z-10 text-white shadow-md">Welcome to Swift MarketPlace!</h2>
+                            <p data-aos="fade-right" data-aos-duration="1500" className="w-2/3 lg:w-1/2 z-20 text-sm md:text-base lg:text-lg xl:text-xl">Your One-Stop Destination for Fashion, Home Decor, and More!</p>
+                            <Link to="/products" data-aos="fade-left" data-aos-duration="1500" data-aos-delay="1000" className="browse-products-btn border-[1px] uppercase h-12 w-64 md:h-9 md:w-72 mt-5 duration-300 font-medium bg-blue-400 hover:bg-blue-200 hover:bg-opacity-70">Browse Products</Link>
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <img src="https://images.pexels.com/photos/2409681/pexels-photo-2409681.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Enjoy the view" className="w-full h-svh object-cover" />
-                </div>
-                <div>
-                    <img src="https://img.freepik.com/free-photo/medium-shot-smiley-woman-with-shopping-bags_23-2149313436.jpg?t=st=1715115812~exp=1715119412~hmac=ab8ea06a53511dc6434a1edebd916c2b401eef4e8f6c7e182e5cefc4714ff052&w=740" alt="Smile after shopping" className="w-full h-svh object-cover" />
-                </div>
-            </Slider>
-            <button onClick={goToPrevSlide} className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-blue-300 p-2 rounded-full shadow-md z-10">
-                <IoMdArrowRoundBack />
-            </button>
-            <button onClick={goToNextSlide} className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-blue-300 p-2 rounded-full shadow-md z-10">
-                <IoMdArrowRoundForward />
-            </button>
-        </div>
+                    <div className='relative'>
+                        <img src="https://img.freepik.com/premium-photo/happy-millennial-african-american-lady-with-laptop-sits-sofa-looks-into-shopping-bag-living-room_116547-35206.jpg?w=740" alt="Enjoy the view" className="w-full h-svh object-cover" />
+                        <div className="absolute inset-0 flex flex-col justify-center items-center text-center text-white">
+                            <h2 data-aos="fade-down" data-aos-duration="1500" className="text-4xl md:text-6xl lg:text-7xl  font-semibold uppercase space-font z-10 text-white shadow-md">Elevate Your Presence with Captivating Fragrances</h2>
+                            <p data-aos="fade-right" data-aos-duration="1500" className="w-2/3 lg:w-1/2 z-20 text-sm md:text-base lg:text-lg xl:text-xl">Shop Exquisite Perfumes</p>
+                            <Link to="/products" data-aos="fade-left" data-aos-duration="1500" data-aos-delay="1000" className="browse-products-btn border-[1px] uppercase h-12 w-64 md:h-9 md:w-72 mt-5 duration-300 font-medium bg-blue-400 hover:bg-blue-200 hover:bg-opacity-70">Browse Products</Link>
+                        </div>
+                    </div>
+                    <div className="relative">
+                        <img src="https://img.freepik.com/free-photo/watch-black-box-bow-tie-lie-white-windowsill_8353-687.jpg?t=st=1715156346~exp=1715159946~hmac=c330d203f13701da055a732decd695b07939e2004720bab95a5759598dda7bdf&w=740" alt="Smile after shopping" className="w-full h-svh object-cover" />
+                        <div className="absolute inset-0 flex flex-col justify-center items-center text-center text-white">
+                            <h2 data-aos="fade-down" data-aos-duration="1500" className="text-4xl md:text-6xl lg:text-7xl  font-semibold uppercase space-font z-10 text-white shadow-md">Explore Our Collection of Watches</h2>
+                            <p data-aos="fade-right" data-aos-duration="1500" className="w-2/3 lg:w-1/2 z-20 text-sm md:text-base lg:text-lg xl:text-xl">Shop Exquisite Perfumes</p>
+                            <Link to="/products" data-aos="fade-left" data-aos-duration="1500" data-aos-delay="1000" className="browse-products-btn border-[1px] uppercase h-12 w-64 md:h-9 md:w-72 mt-5 duration-300 font-medium bg-blue-400 hover:bg-blue-200 hover:bg-opacity-70">Browse Products</Link>
+                        </div>
+                    </div>
+                </Slider>
+                <button onClick={goToPrevSlide} className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-blue-300 p-2 rounded-full shadow-md z-10">
+                    <IoMdArrowRoundBack />
+                </button>
+                <button onClick={goToNextSlide} className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-blue-300 p-2 rounded-full shadow-md z-10">
+                    <IoMdArrowRoundForward />
+                </button>
+            </div>
         </div>
     );
 };
